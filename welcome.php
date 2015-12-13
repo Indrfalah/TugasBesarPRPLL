b14659213fb6f8a93a7e940100e8a30bc5a15c24
indrfalah <indrafallah@yahoo.com>
2015-12-13 10:50:46 +0700

    index edited

---
+++ welcome.php
@@ -0,0 +1,17 @@
+<div>
+    <h1>Hi!!.<?php 
+	if(isset($_SESSION['id_user'])){
+	
+		echo ucwords($_SESSION['username']);
+	
+	}
+	?></h1>
+
+    <p>Situs Tes Potensi Akademik online ini digunakan untuk melaksanakan ujian secara online. user dapat melaksanakan ujian kapan saja dan dimana saja.</p>
+    
+    
+    <p>Sebelum mengikuti ujian, user harus mendaftar sebagai anggota situs ujian online. dan melakukan login sebelum melaksanakan ujian.</p>
+    <p></p>
+    
+</div>
+
